1af5c13cca54f6a4d50f7a4604cff13a5a369ce2
vanloc <vvanloc.vo@gmail.com>
2020-08-22 02:08:27 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Nautilus Open with VSCode
+Dependency to install before: `nautilus-python` (`python-nautilus` package on Debian / Ubuntu)
+## Install 
+- VSCode:
+    ```bash
+    ./install.sh
+    or
+    sudo ./install.sh
+    ```
+- VSCode Insider:
+    ```bash
+    ./install-insiders.sh.sh
+    or
+    sudo ./install-insiders.sh.sh
+    ```
+## Uninstall 
+- Current User:
+    ```bash
+    rm ~/.local/share/nautilus-python/extensions/vscode-nautilus.py
+    or
+    rm ~/.local/share/nautilus-python/extensions/vscodeinsiders-nautilus.py
+    ```
+- All User:
+    ```bash
+    sudo rm /usr/share/nautilus-python/extensions/vscode-nautilus.py
+    or
+    sudo rm /usr/share/nautilus-python/extensions/vscodeinsiders-nautilus.py
+    ```
---
+++ install-insiders.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+
+restart_nautilus()
+{
+    read -p "Do you want restart Nautilus (Files) [Y/N]? " VAR
+    if [[ $VAR = 'y' || $VAR = 'Y' ]]; then
+        nautilus -q
+    fi    
+}
+cp_file() {
+    FILE=vscodeinsiders-nautilus.py
+    TARGDIR=$1
+    if [[ ! -d $TARGDIR ]]; then
+        mkdir -v -p "$TARGDIR"
+    fi
+    cp -v "$FILE" "$TARGDIR"
+    restart_nautilus
+    echo "Sucessfull!"
+}
+cp_file_sudo() {
+    FILE=vscodeinsiders-nautilus.py
+    TARGDIR=$1
+    if [[ ! -d $TARGDIR ]]; then
+        sudo mkdir -v -p "$TARGDIR"
+    fi
+    sudo cp -v "$FILE" "$TARGDIR"
+    restart_nautilus
+    echo "Sucessfull!"
+}
+
+if [[ $UID != 0 ]]; then
+    read -p "This script is running without sudo, install for current user [Y/N]? " VAR
+    if [[ $VAR = 'y' || $VAR = 'Y' ]]; then 
+        TARGDIR=~/.local/share/nautilus-python/extensions
+        cp_file "$TARGDIR"
+    else
+        read -p "Do you want install for all user [Y/N]? " VAR
+        if [[ $VAR = 'y' || $VAR = 'Y' ]]; then 
+            TARGDIR=/usr/share/nautilus-python/extensions
+            cp_file_sudo "$TARGDIR"
+        else
+            echo "Install failed!"
+        fi
+    fi
+    else
+        read -p "This script is running with sudo, install for all user [Y/N]? " VAR
+        if [[ $VAR = 'y' || $VAR = 'Y' ]]; then
+            TARGDIR=/usr/share/nautilus-python/extensions
+            cp_file "$TARGDIR"
+        else
+            echo "Install failed!"
+        fi
+fi
+
